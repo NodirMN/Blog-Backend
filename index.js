2c2725532a41d9fc1906663d9db257f3c71256de
Your Name <81632619+NodirMN@users.noreply.github.com>
2022-07-03 14:10:32 +0500

    file push

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ app.patch(
     PostController.update,
 );
 
-app.listen(process.env.PORT || 4444, (err) => {
+app.listen(process.env.PORT || 44, (err) => {
     if (err) {
         return console.log(err);
     }
